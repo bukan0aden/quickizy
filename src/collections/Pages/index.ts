ae232787a538ffe0fa059355e2aa8198396bf531
adentdk <adentrisnadaudkurnia@gmail.com>
2025-11-03 13:31:06 +0000

    disable autosave

--- src/collections/Pages/index.ts
+++ src/collections/Pages/index.ts
@@ -126,9 +126,7 @@ export const Pages: CollectionConfig<'pages'> = {
   },
   versions: {
     drafts: {
-      autosave: {
-        interval: 100, // We set this interval for optimal live preview
-      },
+      autosave: false,
       schedulePublish: true,
     },
     maxPerDoc: 50,
